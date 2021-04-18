dd501a4268644890347154c5722804af48d3f799
Lautaro Acuña <myspaceinilworld@gmail.com>
2021-04-17 21:30:06 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pySilos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            System.Console.WriteLine("Introduzca la altura del silo");
+            double altura = Convert.ToDouble(Console.ReadLine());
+            System.Console.WriteLine("Introduzca el diametro de la base del silo");
+            double diametroCircular = Convert.ToDouble(Console.ReadLine());
+            Silo silo1 = new Silo(altura, diametroCircular);
+            System.Console.WriteLine("El volumen total del silo es de " + silo1.calcularVolumen()+" cm2");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Silo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pySilos
+{
+    class Silo
+    {
+        private double altura, diametroCircular, radio, area;
+
+        public Silo(double altura, double diametroCircular) {
+            this.altura = altura;
+            this.diametroCircular = diametroCircular;
+            this.radio = diametroCircular / 2;
+            this.area = Math.PI * radio * radio;
+        }
+
+        public double pAltura
+        {
+            set { altura = value; }
+
+            get { return altura; }
+        }
+
+        public double pDiametroCircular
+        {
+            set { diametroCircular = value; }
+
+            get { return diametroCircular; }
+        }
+
+
+        public double calcularVolumen() {
+            return area * altura;
+        }
+      
+    }
+}
